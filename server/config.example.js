419f0cb53d7ad73300d52d76c2886c753070d6bb
Mikdan Jey <mikdanjey@gmail.com>
2021-05-07 20:23:22 +0530

    ok

--- server/config.example.js
+++ server/config.example.js
@@ -13,134 +13,134 @@ const os = require('os');
 module.exports =
 {
 	// Listening hostname (just for `gulp live` task).
-	domain : process.env.DOMAIN || 'localhost',
+	domain: process.env.DOMAIN || 'webrtc1.ithots.tech',
 	// Signaling settings (protoo WebSocket server and HTTP API server).
-	https  :
+	https:
 	{
-		listenIp   : '0.0.0.0',
+		listenIp: '0.0.0.0',
 		// NOTE: Don't change listenPort (client app assumes 4443).
-		listenPort : process.env.PROTOO_LISTEN_PORT || 4443,
+		listenPort: process.env.PROTOO_LISTEN_PORT || 4443,
 		// NOTE: Set your own valid certificate files.
-		tls        :
+		tls:
 		{
-			cert : process.env.HTTPS_CERT_FULLCHAIN || `${__dirname}/certs/fullchain.pem`,
-			key  : process.env.HTTPS_CERT_PRIVKEY || `${__dirname}/certs/privkey.pem`
+			cert: process.env.HTTPS_CERT_FULLCHAIN || `${__dirname}/certs/fullchain.pem`,
+			key: process.env.HTTPS_CERT_PRIVKEY || `${__dirname}/certs/privkey.pem`
 		}
 	},
 	// mediasoup settings.
-	mediasoup :
+	mediasoup:
 	{
 		// Number of mediasoup workers to launch.
-		numWorkers     : Object.keys(os.cpus()).length,
+		numWorkers: Object.keys(os.cpus()).length,
 		// mediasoup WorkerSettings.
 		// See https://mediasoup.org/documentation/v3/mediasoup/api/#WorkerSettings
-		workerSettings :
+		workerSettings:
 		{
-			logLevel : 'warn',
-			logTags  :
-			[
-				'info',
-				'ice',
-				'dtls',
-				'rtp',
-				'srtp',
-				'rtcp',
-				'rtx',
-				'bwe',
-				'score',
-				'simulcast',
-				'svc',
-				'sctp'
-			],
-			rtcMinPort : process.env.MEDIASOUP_MIN_PORT || 40000,
-			rtcMaxPort : process.env.MEDIASOUP_MAX_PORT || 49999
+			logLevel: 'warn',
+			logTags:
+				[
+					'info',
+					'ice',
+					'dtls',
+					'rtp',
+					'srtp',
+					'rtcp',
+					'rtx',
+					'bwe',
+					'score',
+					'simulcast',
+					'svc',
+					'sctp'
+				],
+			rtcMinPort: process.env.MEDIASOUP_MIN_PORT || 40000,
+			rtcMaxPort: process.env.MEDIASOUP_MAX_PORT || 49999
 		},
 		// mediasoup Router options.
 		// See https://mediasoup.org/documentation/v3/mediasoup/api/#RouterOptions
-		routerOptions :
+		routerOptions:
 		{
-			mediaCodecs :
-			[
-				{
-					kind      : 'audio',
-					mimeType  : 'audio/opus',
-					clockRate : 48000,
-					channels  : 2
-				},
-				{
-					kind       : 'video',
-					mimeType   : 'video/VP8',
-					clockRate  : 90000,
-					parameters :
+			mediaCodecs:
+				[
 					{
-						'x-google-start-bitrate' : 1000
-					}
-				},
-				{
-					kind       : 'video',
-					mimeType   : 'video/VP9',
-					clockRate  : 90000,
-					parameters :
+						kind: 'audio',
+						mimeType: 'audio/opus',
+						clockRate: 48000,
+						channels: 2
+					},
 					{
-						'profile-id'             : 2,
-						'x-google-start-bitrate' : 1000
-					}
-				},
-				{
-					kind       : 'video',
-					mimeType   : 'video/h264',
-					clockRate  : 90000,
-					parameters :
+						kind: 'video',
+						mimeType: 'video/VP8',
+						clockRate: 90000,
+						parameters:
+						{
+							'x-google-start-bitrate': 1000
+						}
+					},
 					{
-						'packetization-mode'      : 1,
-						'profile-level-id'        : '4d0032',
-						'level-asymmetry-allowed' : 1,
-						'x-google-start-bitrate'  : 1000
-					}
-				},
-				{
-					kind       : 'video',
-					mimeType   : 'video/h264',
-					clockRate  : 90000,
-					parameters :
+						kind: 'video',
+						mimeType: 'video/VP9',
+						clockRate: 90000,
+						parameters:
+						{
+							'profile-id': 2,
+							'x-google-start-bitrate': 1000
+						}
+					},
 					{
-						'packetization-mode'      : 1,
-						'profile-level-id'        : '42e01f',
-						'level-asymmetry-allowed' : 1,
-						'x-google-start-bitrate'  : 1000
+						kind: 'video',
+						mimeType: 'video/h264',
+						clockRate: 90000,
+						parameters:
+						{
+							'packetization-mode': 1,
+							'profile-level-id': '4d0032',
+							'level-asymmetry-allowed': 1,
+							'x-google-start-bitrate': 1000
+						}
+					},
+					{
+						kind: 'video',
+						mimeType: 'video/h264',
+						clockRate: 90000,
+						parameters:
+						{
+							'packetization-mode': 1,
+							'profile-level-id': '42e01f',
+							'level-asymmetry-allowed': 1,
+							'x-google-start-bitrate': 1000
+						}
 					}
-				}
-			]
+				]
 		},
 		// mediasoup WebRtcTransport options for WebRTC endpoints (mediasoup-client,
 		// libmediasoupclient).
 		// See https://mediasoup.org/documentation/v3/mediasoup/api/#WebRtcTransportOptions
-		webRtcTransportOptions :
+		webRtcTransportOptions:
 		{
-			listenIps :
-			[
-				{
-					ip          : process.env.MEDIASOUP_LISTEN_IP || '1.2.3.4',
-					announcedIp : process.env.MEDIASOUP_ANNOUNCED_IP
-				}
-			],
-			initialAvailableOutgoingBitrate : 1000000,
-			minimumAvailableOutgoingBitrate : 600000,
-			maxSctpMessageSize              : 262144,
+			listenIps:
+				[
+					{
+						ip: process.env.MEDIASOUP_LISTEN_IP || '139.59.78.193',
+						announcedIp: process.env.MEDIASOUP_ANNOUNCED_IP
+					}
+				],
+			initialAvailableOutgoingBitrate: 1000000,
+			minimumAvailableOutgoingBitrate: 600000,
+			maxSctpMessageSize: 262144,
 			// Additional options that are not part of WebRtcTransportOptions.
-			maxIncomingBitrate              : 1500000
+			maxIncomingBitrate: 1500000
 		},
 		// mediasoup PlainTransport options for legacy RTP endpoints (FFmpeg,
 		// GStreamer).
 		// See https://mediasoup.org/documentation/v3/mediasoup/api/#PlainTransportOptions
-		plainTransportOptions :
+		plainTransportOptions:
 		{
-			listenIp :
+			listenIp:
 			{
-				ip          : process.env.MEDIASOUP_LISTEN_IP || '1.2.3.4',
-				announcedIp : process.env.MEDIASOUP_ANNOUNCED_IP
+				ip: process.env.MEDIASOUP_LISTEN_IP || '139.59.78.193',
+				announcedIp: process.env.MEDIASOUP_ANNOUNCED_IP
 			},
-			maxSctpMessageSize : 262144
+			maxSctpMessageSize: 262144
 		}
 	}
 };
